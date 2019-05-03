33abf4040a600cb9f29b8c2e94a6a1f21c37cb2c
csh919 <csh919@DESKTOP-09I5B1Q>
2019-05-04 01:48:24 +0900

    test

--- framework-dependencies/framework-parent/framework-config-server/src/main/java/me/devstar/config/Application.java
+++ framework-dependencies/framework-parent/framework-config-server/src/main/java/me/devstar/config/Application.java
@@ -6,16 +6,24 @@ package me.devstar.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author sudden(sch0718@naver.com)
  */
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
 	}
 	
 }
