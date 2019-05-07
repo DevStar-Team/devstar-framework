77dd740928fc20457798283f9b1c5b114dd4752f
shjo <shjo@mzc-shjo>
2019-05-07 12:42:03 +0900

    rollback

--- framework-dependencies/framework-parent/framework-config-server/src/main/java/me/devstar/config/Application.java
+++ framework-dependencies/framework-parent/framework-config-server/src/main/java/me/devstar/config/Application.java
@@ -5,15 +5,16 @@ package me.devstar.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author sudden(sch0718@naver.com)
  */
 @SpringBootApplication
+@EnableConfigServer
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
 }
